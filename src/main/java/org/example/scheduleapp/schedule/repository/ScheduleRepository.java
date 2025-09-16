5acd58b19a4a8f56ba0c2bb6b14398f7b686ebf6
grace-jsy <sseo.oee@gmail.com>
2025-09-16 15:16:43 +0900

    feat: create ScheduleRepository interface

---
+++ src/main/java/org/example/scheduleapp/schedule/repository/ScheduleRepository.java
@@ -0,0 +1,8 @@
+package org.example.scheduleapp.schedule.repository;
+
+
+import org.example.scheduleapp.schedule.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+}
