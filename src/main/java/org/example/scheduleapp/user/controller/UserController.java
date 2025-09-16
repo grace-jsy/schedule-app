e4920bafd6dcd080722220b9b191cdcf5977bfa6
grace-jsy <sseo.oee@gmail.com>
2025-09-16 21:12:51 +0900

    feat: get current user info

--- src/main/java/org/example/scheduleapp/user/controller/UserController.java
+++ src/main/java/org/example/scheduleapp/user/controller/UserController.java
@@ -1,10 +1,11 @@
 package org.example.scheduleapp.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.scheduleapp.common.consts.Const;
+import org.example.scheduleapp.user.dto.UserResponseDto;
 import org.example.scheduleapp.user.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,5 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/me") // 내 정보 조회
+    public ResponseEntity<UserResponseDto> getMe(@SessionAttribute(name = Const.LOGIN_USER) Long id) {
+
+        userService.getMe(id);
+    }
 
 }
--- src/main/java/org/example/scheduleapp/user/service/UserService.java
+++ src/main/java/org/example/scheduleapp/user/service/UserService.java
@@ -1,12 +1,26 @@
 package org.example.scheduleapp.user.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.example.scheduleapp.user.dto.UserResponseDto;
+import org.example.scheduleapp.user.entity.User;
 import org.example.scheduleapp.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional // 내 정보 조회
+    public UserResponseDto getMe(Long id) {
+
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Not found user")
+        );
+
+        return new UserResponseDto(user.getId(), user.getUsername(), user.getEmail());
+    }
 }
