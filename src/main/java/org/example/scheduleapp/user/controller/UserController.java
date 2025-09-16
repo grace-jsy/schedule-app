1236d789f65be0b5b53c0126f71c63787365de42
grace-jsy <sseo.oee@gmail.com>
2025-09-16 21:28:59 +0900

    feat: add user update in service and controller

--- src/main/java/org/example/scheduleapp/user/controller/UserController.java
+++ src/main/java/org/example/scheduleapp/user/controller/UserController.java
@@ -2,6 +2,7 @@ package org.example.scheduleapp.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.scheduleapp.common.consts.Const;
+import org.example.scheduleapp.user.dto.UpdateUserRequestDto;
 import org.example.scheduleapp.user.dto.UserResponseDto;
 import org.example.scheduleapp.user.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -21,6 +22,14 @@ public class UserController {
         UserResponseDto userResponseDto = userService.getMe(id);
 
         return new ResponseEntity<>(userResponseDto, HttpStatus.OK);
+    }
+
+    @PutMapping ("/me")// 내 정보 수정
+    public ResponseEntity<UserResponseDto> updateUser(
+            @SessionAttribute(name = Const.LOGIN_USER) Long id,
+            @RequestBody UpdateUserRequestDto request) {
+
+        UserResponseDto userResponseDto = userService.updateUser(id, request.getPassword());
     }
 
 }
--- src/main/java/org/example/scheduleapp/user/service/UserService.java
+++ src/main/java/org/example/scheduleapp/user/service/UserService.java
@@ -2,6 +2,7 @@ package org.example.scheduleapp.user.service;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.example.scheduleapp.common.config.PasswordEncoder;
 import org.example.scheduleapp.user.dto.UserResponseDto;
 import org.example.scheduleapp.user.entity.User;
 import org.example.scheduleapp.user.repository.UserRepository;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional // 내 정보 조회
     public UserResponseDto getMe(Long id) {
@@ -22,5 +24,21 @@ public class UserService {
         );
 
         return new UserResponseDto(user.getId(), user.getUsername(), user.getEmail());
+    }
+
+    @Transactional // 내 정보 수정
+    public UserResponseDto updateUser(Long id, String inputPassword) {
+
+        String encodedPassword = passwordEncoder.encode(inputPassword);
+
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Not found user")
+        );
+
+        user.updateUser(encodedPassword);
+
+        User savedUser = userRepository.save(user);
+
+        return new UserResponseDto(savedUser.getId(), savedUser.getUsername(), savedUser.getEmail());
     }
 }
